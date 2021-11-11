580eaf6e041e6848f92caf9767d1571fa266d07a
Çağlar Çavdar <caglarcavdar@trex.com.tr>
2021-11-11 18:20:39 +0300

    able to read data from modbus

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using NModbus;
+using System;
+using System.Net.Sockets;
+using System.Threading;
 
 namespace LearningModbus
 {
@@ -6,7 +9,20 @@ namespace LearningModbus
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var modbusFactory = new ModbusFactory();
+            var tcpClient = new TcpClient();
+            IModbusMaster master = modbusFactory.CreateMaster(tcpClient);
+            master.Transport.Retries = 3;
+            master.Transport.WaitToRetryMilliseconds = 1000;
+            master.Transport.SlaveBusyUsesRetryCount = true;
+            tcpClient.ConnectAsync("127.0.0.1", 10502);
+            while (true)
+            {
+                Thread.Sleep(3000);
+                var result = master.ReadHoldingRegisters(1, 0, 1);
+                Console.WriteLine(result[0]);
+            }
+            Console.ReadLine();
         }
     }
 }
